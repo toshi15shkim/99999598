b2faac2e7fd237a32b84824fdc2167963a521b34
김성현 <sung_hyun.kim@kt.com>
2024-11-15 13:42:28 +0900

    restcontroller 변경

--- src/main/java/com/example/demo/controller/AaController.java
+++ src/main/java/com/example/demo/controller/AaController.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class TodoController {
+@RestController
+public class AaController {
 
-    @GetMapping("/")
-    public String home() {
-        return "todoList";
+    @GetMapping("/api/v1/user")
+    public String user() {
+        return "ktds 82265611";
     }
-
 }
